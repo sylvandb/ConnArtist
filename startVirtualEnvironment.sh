e386f6718d067e54b1d4a780b980c27e1a6661ee
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2020-05-27 11:30:08 -0600

    run as 'connartist' on all IPs

--- startVirtualEnvironment.sh
+++ startVirtualEnvironment.sh
@@ -3,9 +3,14 @@ test -e ./ConnArtist.py || { echo "Missing ConnArtist, wrong dir?" >&2; exit 1;}
 
 test -d flaskVENV || ./createVirtualEnvironment.sh || exit 2
 
+if [ $(id -un) != connartist ]; then
+ sudo -p "Enter your password to run as connartist: " -u connartist $0
+ exit
+fi
+
 echo "$PS1" | grep -q '(flaskVENV)' || . flaskVENV/bin/activate
 echo "$PS1" | grep -q '(flaskVENV)' || { echo "fail venv" >&2; exit 3;}
 
 flask --version 2>&1 >/dev/null || pip install flask
 
-FLASK_APP=ConnArtist.py flask run
+FLASK_APP=ConnArtist.py flask run --host=0.0.0.0
