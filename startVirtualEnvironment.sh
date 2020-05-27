cd520a5ead8c7c782ad890387a053818207d98d7
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2020-05-27 11:15:27 -0600

    flesh out start venv

--- startVirtualEnvironment.sh
+++ startVirtualEnvironment.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+test -e ./ConnArtist.py || { echo "Missing ConnArtist, wrong dir?" >&2; exit 1;}
 
-source flaskVENV/bin/activate
+test -d flaskVENV || ./createVirtualEnvironment.sh || exit 2
+
+echo "$PS1" | grep -q '(flaskVENV)' || . flaskVENV/bin/activate
+echo "$PS1" | grep -q '(flaskVENV)' || { echo "fail venv" >&2; exit 3;}
+
+flask --version 2>&1 >/dev/null || pip install flask
+
+FLASK_APP=ConnArtist.py flask run
